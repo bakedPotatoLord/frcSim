2877fa6b59e6ebf87b6943ff5657a771138956b8
= <918780@dpsk12.net>
2022-10-01 16:04:12 -0600

    added istouchingArr funct

--- BaseClass.js
+++ BaseClass.js
@@ -12,6 +12,15 @@ class BaseClass{
 		return (objectDist(this,object)) <= (this.r +object.r);
 	}
 
+	istouchingArr(objectArr){
+		for(let k of objectArr){
+			if (this.isTouching(k)){
+				return true
+			}
+		}
+		return false
+	}
+
 	isTouchingWall(){
 		return (this.x+ this.r < 0) || (this.x+ this.r > cw) ||(this.y+ this.r < 0) || (this.y+ this.r > ch)
 	}
