649eadb003e5bebcb487453b9cfdaac254ad5eeb
= <918780@dpsk12.net>
2022-10-02 13:59:59 -0600

    changed color scheme

--- Hub.js
+++ Hub.js
@@ -1,16 +1,22 @@
-class Hub extends BaseClass{
+class Hub extends BaseClass {
     constructor(){
         super()
         this.x = cw/2
         this.y = ch/2
         this.r = 50
+        
     }
 
 
     draw(){
-        ctx.fillStyle = 'black'
+        ctx.fillStyle = 'rgb(200,200,200)'
 		ctx.beginPath();
 		ctx.arc(this.x,this.y,this.r,0,Tao)
+		ctx.fill() 
+
+        ctx.fillStyle = 'rgb(100,100,100)'
+        ctx.beginPath();
+		ctx.arc(this.x,this.y,this.r-10,0,Tao)
 		ctx.fill() 
     }
 }
